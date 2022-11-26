4aaa100b6c1ac3da28d8f5731285888e065f1fef
[Shann Roberts] <[dpr.1979@bigpond.com]>
2022-11-26 20:11:29 +0800

    add node_modules folder

---
+++ myAPI.js
@@ -0,0 +1,34 @@
+//create a local server using express that listens to port '3000'.
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World! Updated message')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
+
+//in the terminal
+// $ npm start 
+
+// returns
+
+// > expensetracker@1.0.0 start
+// > node myAPI.js
+
+// Example app listening on port 3000!
+
+//then
+
+//in browser type localhost:3000 (route URL)
+//to see Hello World! message on screen
+//any other path eg. localhost:3000/test
+// will return 404 status not found / cannot get test
+
+//make sure nodemon is included in package.json "start"
+//then in terminal $npm start
+//any updates to myAPI.js will then appear in browser on refresh 
